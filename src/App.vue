6d9db010eefbc3d6ff6b8e818fac749b1b32f00d
1108Famiglistimo <2439214028@qq.com>
2022-06-23 20:26:36 +0800

    决定灿烂

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="当你决定灿烂，山无拦，海无遮。"/>
   </div>
 </template>
 
